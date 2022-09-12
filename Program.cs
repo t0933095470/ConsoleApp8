536172a3c2a3dee0b834c554f9bb23be0ddd7345
t0933095470 <thethao165@gmail.com>
2022-09-12 09:31:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// If else structure
+
+string a;
+Console.WriteLine("Please enter Vu_Ngoc_Tan");
+a = Console.ReadLine();
+
+if (a == "Vu_Ngoc_Tan")
+{
+    Console.Write("Day la Tan.");
+}
+else
+{
+    Console.WriteLine("Day khong phai la Tan.");
+}
